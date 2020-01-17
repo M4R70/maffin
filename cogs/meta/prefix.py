9878037903ce632b016d9343b612be8757d20f7a
marto <martinjaltman@gmail.com>
2020-01-17 16:56:40 -0300

    perm checks

---
+++ cogs/meta/prefix.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from discord.ext import commands
+import discord
+import utils.db
+
+
+class prefix(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.has_permissions(administrator=True)
+    @commands.command()
+    async def set_prefix(self, ctx, prefix: str):
+        await utils.db.updateOne('prefixes', {'guild_id': ctx.guild.id}, {'$set': {'prefix': prefix}})
+        await ctx.send('Prefix changed :thumbsup:')
+
+    @commands.command()
+    async def get_prefix(self, ctx):
+        db_info = await utils.db.findOne('prefixes', {'guild_id': message.guild.id})
+        res = ''
+        if db_info is None:
+           res = default_prefix
+        else:
+            res = db_info['prefix']
+
+        await ctx.send("This guild's pefix is " + res)
+
+def setup(bot):
+    bot.add_cog(prefix(bot))
