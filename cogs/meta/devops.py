9893ac500f489ac7e92186be606686f39c80ca93
M4R70 <martinjaltman@gmail.com>
2020-12-31 11:52:06 -0300

    Update devops.py

--- cogs/meta/devops.py
+++ cogs/meta/devops.py
@@ -7,31 +7,9 @@ from utils.checks import is_host, is_cog_enabled, is_allowed_in_config, dev
 import sys
 import cloudpickle
 
-from types import ModuleType, FunctionType
-from gc import get_referents
+from operator import itemgetter
 
-# Custom objects know their class.
-# Function objects seem to know way too much, including modules.
-# Exclude modules as well.
-BLACKLIST = type, ModuleType, FunctionType
-
-
-def getsize(obj):
-    """sum size of object & members."""
-    if isinstance(obj, BLACKLIST):
-        raise TypeError('getsize() does not take argument of type: '+ str(type(obj)))
-    seen_ids = set()
-    size = 0
-    objects = [obj]
-    while objects:
-        need_referents = []
-        for obj in objects:
-            if not isinstance(obj, BLACKLIST) and id(obj) not in seen_ids:
-                seen_ids.add(id(obj))
-                size += sys.getsizeof(obj)
-                need_referents.append(obj)
-        objects = get_referents(*need_referents)
-    return size
+from pympler import tracker
 
 class DevOps(commands.Cog):
 	def __init__(self, bot):
@@ -45,11 +23,8 @@ class DevOps(commands.Cog):
 	@dev()
 	@commands.command()
 	async def mem_check(self, ctx):
-		res = ":) \n"
-		for cog_name,cog in self.bot.cogs.items():
-			res += cog_name + " " + str(getsize(cog)) + '\n'
-		await ctx.send(res)
-
+		mem = tracker.SummaryTracker()
+		print(sorted(mem.create_summary(), reverse=True, key=itemgetter(2))[:10])
 
 def setup(bot):
 	bot.add_cog(DevOps(bot))
